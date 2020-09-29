3bcbfc593f032bc470fa63b4245ffa99e0d4b832
arielmerin <arielmerino@ciencias.unam.mx>
2020-09-29 12:48:02 -0500

    Primera clase de laboratorio

---
+++ ModProg/hello.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#
+
+
+int main()
+{
+    int entero = 3;
+    float flotante = 5.9;
+    double doble = 5.84;
+    float suma = flotante + doble;
+    int a = 0, b = 3, c = 5;
+    
+    printf("La suma de entero, flotante y doblre, es %.2f \n", suma);
+    return 0;
+}
+/**
+ * Deinición de booleanos
+ */
+
+/**
+ * Definir matrices en C
+ */
+/**
+ * Indicamos que estamos definiendo un arreglo del 1 al 10
+ * int numeros[10];z
